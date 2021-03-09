9c1742be3733d9476ac76c4f06c80576434097e0
Wim <w.j.stienstra@home.nl>
2021-03-09 12:41:42 +0100

    Added fix for live reload

--- MileAway/Startup.cs
+++ MileAway/Startup.cs
@@ -38,6 +38,7 @@ namespace MileAway
             {
                 app.UseDeveloperExceptionPage();
                 app.UseLiveReload();
+                app.UseBrowserLink();
             }
             else
             {
