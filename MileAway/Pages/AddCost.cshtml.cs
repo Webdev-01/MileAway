f3ca02fd46a3161fd0e1e98a10cc767eb2fd0eb4
Romme <romme.schotanus@gmail.com>
2021-03-09 16:05:32 +0100

    Halve add cost

--- MileAway/Pages/AddCost.cshtml.cs
+++ MileAway/Pages/AddCost.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,14 @@ namespace MileAway.Pages
 {
     public class AddCostModel : PageModel
     {
-        public void OnGet()
+        public string costType { get; set; }
+        public string costTypeString { get; set; }
+
+        public ActionResult OnGet()
         {
+            costType = HttpContext.Request.Query["type"];
+            costTypeString = costType == "brandstof" ? "selected" : "";
+            return Page();
         }
     }
 }
