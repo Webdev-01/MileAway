ef5f44c430a8b01dfd554ca8271bbeecc69e8afb
Romme <romme.schotanus@gmail.com>
2021-03-16 13:15:16 +0100

    Added CostsRepository + code format

---
+++ MileAway/Repositories/CostsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using MySql.Data.MySqlClient;
+using MileAway.Models;
+
+namespace MileAway.Repositories
+{
+    public class CostsRepository
+    {
+        private static IDbConnection Connect()
+        {
+            return new MySqlConnection(
+                "Server = localhost;" +
+                "Port = 3306;" +
+                "Database = mileaway;" +
+                "Uid = root;" +
+                "Pwd =;");
+        }
+    }
+}
