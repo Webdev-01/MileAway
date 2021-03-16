1ccf96ce680a6598ef09e78e2801a9a18762f18f
Romme <romme.schotanus@gmail.com>
2021-03-16 12:56:43 +0100

    Reparatie forum toegevoegd + asp-for

--- MileAway/Pages/AddCost.cshtml.cs
+++ MileAway/Pages/AddCost.cshtml.cs
@@ -2,15 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MileAway.Models;
+using MileAway.Repositories;
 
 namespace MileAway.Pages
 {
     public class AddCostModel : PageModel
     {
         public string costType { get; set; }
+
+        [BindProperty]
+        public Costs Costs { get; set; }
+        [BindProperty]
+        public Vehicles Vehicles { get; set; }
 
         public ActionResult OnGet()
         {
