dd3aa0ad873b28d1aa9028501a8cb6490e6fe571
hendrydevries <26hendrydevries2000@gmail.com>
2021-03-16 10:37:15 +0100

    Update Users.cs

--- MileAway/Models/Users.cs
+++ MileAway/Models/Users.cs
@@ -10,9 +10,11 @@ namespace MileAway.Models
         public string EMAIL { get; set; }
 
         [Required]
+        [StringLength(100, ErrorMessage = "{0} length must be at least {2} characters long.", MinimumLength = 6)]
         public string PASSWORD { get; set; }
 
-        [Compare("PASSWORD")]
+        [Required]
+        [Compare("password", ErrorMessage = "The passwords do not match with eachother.")]
         public string CONFIRMPASSWORD { get; set; }
 
         [Required]
